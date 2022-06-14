8d560c602add28753510383e2f0041c87d10101b
Evgenij Kharitonov <evgenijlord@gmail.com>
2022-06-14 20:39:40 +0300

    ucFirst

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,9 +9,17 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
+
+
+function isValid(name) { 
+  if (!name || name.includes(' ') || name.length < 4) {
+    return false;
+  } else {
+    return true;
+  }
 }
+
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
@@ -22,3 +30,4 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+sayHello();
